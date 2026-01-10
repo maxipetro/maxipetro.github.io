bd1c53fde1e0c403f04a975cdd654dfaad180684
heimerzieher <heimerzieher@me.com>
2026-01-10 17:48:37 +0100

    Added information

--- research.php
+++ research.php
@@ -1,5 +1,18 @@
 <?php $selected = 'reserach'; include 'header.php';?>
 
-Reserach
+<b>Preprints</b>
+
+Elementary Infinity-Toposes from Type Theory, <a href="https://arxiv.org/abs/2512.18891">arxiv: 2512.18891</a>
+
+<br>
+
+<b>Talks</b>
+
+Elementary Infinity-Toposes from Type Theory, CMS Summer Meeting, Session: Category Theory: Structures and Applications, Université Laval, Quebec City, June 8, 2025
+
+Elementary Infinity-Toposes from Type Theory, 32nd Foundational Methods in Computer Science Workshop, University of Ottawa, Ottawa, June 20, 2025
+
+Nominalism and Infinite Cardinality, ic.SoAP/EENPS, Belgrade and Geneva/Lugano, 2021
+
 
 <?php include 'footer.php'; ?>
--- teaching.php
+++ teaching.php
@@ -1,5 +1,24 @@
 <?php $selected = 'teaching'; include 'header.php';?>
 
-This is my teaching
+<b>Co-Teacher:</b>
+
+“Basic Logic”, Fall and Winter 2025/26, Western University
+
+<br>
+
+<b>Teaching Assistant:</b>
+
+“Calculus II”, Summer 2025, Western University
+
+“Calculus I”, Winter 2025, Western University
+
+“Methods of Matrix Algebra”, Winter 2025, Western University
+
+“Discrete Structures for Engineering”, Fall 2024, Western University
+
+“Basic Logic”, Fall and Winter 2023/24, Western University
+
+“Basic Logic”, Fall and Winter 2022/23, Western University
+
 
 <?php include 'footer.php'; ?>
