7c1ba664c4704c8099ef000f03cd3655a3aaf241
heimerzieher <heimerzieher@me.com>
2026-01-10 17:54:04 +0100

    Added line breaks

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <div class="d-flex">
     <div class="flex-grow-1 ms-3">
     <p>I am a PhD at student at <a href="https://www.uwo.ca">Western University</a> interested in (higher) category theory, logic and type theory and homotopical algebra. My supervisors are Chris Kapulkin (Department of Mathematics) and John Bell (Department of Philosophy). I obtained a MSc in Mathematics from Western and an MA in Philosophy from <a href="https://www.usi.ch/en/education/master/philosophy">Università della Svizzera italiana, Lugano, Switzerland</a>.</p>
-
+    <br>
     <p>Currently, I am working on the interpretation and semantics of univalent dependent type theories in infinity-categories. Part of my research is developing a semantic framework for univalent type theories in elementary infinity toposes. These type theories are also sometimes summarised under the name ‘Homotopy Type Theory’.</p>
-
+    <br>
     <p>I am also interested in higher category theory and homotopical algebra in general, in studying different models of infinity categories and studying model categories. On the philosophical side, I am interested in constructive mathematics.</p>
-
+    <br>
     <p>Here, you can find out about my <a href="/research.html">research</a>, CV and contact.</p>
     </div>
 </div>
