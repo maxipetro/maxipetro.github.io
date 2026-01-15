9fb9cb8e54e83c515bb99d8278a52eb3255704e5
heimerzieher <heimerzieher@me.com>
2026-01-15 19:10:59 +0100

    Updates

--- header.php
+++ header.php
@@ -13,7 +13,6 @@
         <?php if($selected == 'index') { ?>
 
         <?php }?>
-
-        <div class="container shadow-lg pl-5 pr-5 pt-1 pb-5 mb-3 bg-white rounded" id="mainContainer">
+        <div class="container shadow-lg p-5 mt-5 mb-3 bg-white rounded" id="mainContainer">
             <h1>Maximilian Petrowitsch</h1>
             <?php include 'navbar.php';?>
