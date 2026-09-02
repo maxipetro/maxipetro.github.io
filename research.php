aebe4e55ee5cc6fb1e49839faaca0511a7e91332
heimerzieher <heimerzieher@me.com>
2026-09-02 14:53:10 +0200

    Added links and fixed date format

--- research.php
+++ research.php
@@ -12,8 +12,8 @@
 
             <p>I am contributor of the <a href="https://github.com/HoTT/Coq-HoTT">Coq-HoTT Library</a>. </p>
 
-            <p><i>Integers as Higher Inductive Type</i> [<a href="https://github.com/HoTT/Coq-HoTT/pull/2384" >PR</a>, <a href="https://github.com/HoTT/Coq-HoTT/commit/c689d34896a4a6edb3c2143169b39d63137ebc81">code</a>] (jww. Dan Christensen)</p>
-            <p><i>Biinvertible maps</i> [<a href="https://github.com/HoTT/Coq-HoTT/pull/2362" >PR</a>, <a href="https://github.com/maxipetro/Coq-HoTT/commit/090ebd3c5049c8ff15a799428b92ea2f9b84d0b4">code</a>] (jww. Dan Christensen)</p>
+            <p><i>Integers as Higher Inductive Type</i> [<a href="https://github.com/HoTT/Coq-HoTT/pull/2384" >PR</a>, <a href="https://github.com/HoTT/Coq-HoTT/commit/c689d34896a4a6edb3c2143169b39d63137ebc81">code</a>] (jww. <a href="https://jdc.math.uwo.ca/">Dan Christensen</a>)</p>
+            <p><i>Biinvertible maps</i> [<a href="https://github.com/HoTT/Coq-HoTT/pull/2362" >PR</a>, <a href="https://github.com/maxipetro/Coq-HoTT/commit/090ebd3c5049c8ff15a799428b92ea2f9b84d0b4">code</a>] (jww. <a href="https://jdc.math.uwo.ca/">Dan Christensen</a>)</p>
 
             <br>
 
@@ -27,30 +27,30 @@
 
             <i>Contributed:</i>
 
-            <p> Category Theory 2026 (International Congress of Mathematicians), Johns Hopkins University, Baltimore, July 16, 2026</p>
+            <p> Category Theory 2026 (International Congress of Mathematicians), Johns Hopkins University, Baltimore, 16 July 2026</p>
 
-            <p> 3rd Workshop on Syntax and Semantics of Type Theory, Faculty of Mathematics and Physics, Ljubljana, June 4, 2026</p>
+            <p> 3rd Workshop on Syntax and Semantics of Type Theory, Faculty of Mathematics and Physics, Ljubljana, 4 June 2026</p>
 
-            <p> Workshop on Homotopy Type Theory / Univalent Foundations 2026, Aarhus University, Aarhus, June 2, 2026</p>
+            <p> Workshop on Homotopy Type Theory / Univalent Foundations 2026, Aarhus University, Aarhus, 2 June 2026</p>
 
-            <p> 32nd Foundational Methods in Computer Science Workshop, University of Ottawa, Ottawa, June 20, 2025</p>
+            <p> 32nd Foundational Methods in Computer Science Workshop, University of Ottawa, Ottawa, 20 June 2025</p>
 
             <i>Invited:</i>
 
-            <p>Canadian Mathematical Society Summer Meeting, Session: Category Theory: Structures and Applications, Université Laval, Quebec City, June 8, 2025</p>
+            <p>Canadian Mathematical Society Summer Meeting, Session: Category Theory: Structures and Applications, Université Laval, Quebec City, 8 June 2025</p>
             
             <br>
 
             <b>The Yoneda Lemma as a Principle of Structuralist Mathematics</b>
 
-            <p>Research Seminar, Lugano, November 21, 2025</p>
+            <p>Research Seminar, Lugano, 21 November 2025</p>
 
             <br>
         
             <b>Nominalism and Infinite Cardinality</b>
 
-            <p>ic.SoAP, Geneva, June 12, 2021</p>
+            <p>ic.SoAP, Geneva, 12 June 2021</p>
 
-            <p>EENPS, Belgrade, May 1, 2021</p>
+            <p>EENPS, Belgrade, 1 May 2021</p>
 
 <?php include 'footer.php'; ?>
